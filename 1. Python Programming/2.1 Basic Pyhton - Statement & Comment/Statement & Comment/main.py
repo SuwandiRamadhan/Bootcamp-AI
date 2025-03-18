133c8ff8da952b8c4efb046887371163d83944e8
AnonymousXbkl <kerjaandi01@gmail.com>
2025-03-18 23:00:16 +0700

    statement&comment

---
+++ 1. Python Programming/2.1 Basic Pyhton - Statement & Comment/Statement & Comment/main.py
@@ -0,0 +1,18 @@
+# Contoh statement pada bahasa pemrograman python (perhatikan indentasi !)
+batasMaksmimum = 1
+
+for i in range(1, batasMaksmimum):
+    print(i)
+    if i = 5:
+        break
+
+if true:
+    print('Hello World'); a = 5
+
+
+
+
+
+
+
+

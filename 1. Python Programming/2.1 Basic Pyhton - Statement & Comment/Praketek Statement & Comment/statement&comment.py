895349d0234b1b12e93f06c124d8ad76489df06e
AnonymousXbkl <kerjaandi01@gmail.com>
2025-03-20 04:33:36 +0700

    Operator

--- 1. Python Programming/2.1 Basic Pyhton - Statement & Comment/Praketek Statement & Comment/statement&comment.py
+++ 1. Python Programming/2.1 Basic Pyhton - Statement & Comment/Praketek Statement & Comment/statement&comment.py
@@ -1,15 +1,13 @@
 # Contoh statement pada bahasa pemrograman python (perhatikan indentasi !)
 batasMaksmimum = 1
-
 for i in range(1, batasMaksmimum):
     print(i)
-    if i = 5:
+    if i == 5:
         break
 
-if true:
-    print('Hello World'); a = 5
+if True: print('Hello'); a = 5
 
-
+print('\nHello World')
 
 
 
